1185038260c1cb3c45b153f2e1483a685ca2b5bb
ieztebann <114705067+ieztebann@users.noreply.github.com>
2024-12-03 14:01:11 -0500

    Actualizo modelo para corregir los metodos de consulta etc Actualizacion continua

--- routes/vtest/controller/Vehicle.js
+++ routes/vtest/controller/Vehicle.js
@@ -1,27 +1,38 @@
 const bcrypt = require('bcryptjs');
-const { Vehicle } = require('../../../models'); // Importa tus modelos de Sequelize
-
+const { Vehicle, ExternalVehicle } = require('../../../models'); // Importa tus modelos de Sequelize
 /**
  * Función para gestionar una persona, crea o actualiza registros en la base de datos.
- * @param {Object} datosProducto - Datos de la persona a crear o actualizar.
+ * @param {Object} datosVehiculo - Datos de la persona a crear o actualizar.
  * @param {number} idUsuario - ID del usuario que realiza la operación.
  * @param {string} dbDate - Fecha formateada actual en formato "YYYY-MM-DD HH:mm:ss".
  * @returns {Promise<Object>} - Retorna el objeto Persona creado o actualizado.
  */
-const getVehicle = async (Plate) => {
+const manageVehicle = async (datosVehiculo, idPersona, idUsuario, transaction) => {
+    let currentVehicle;
     try {
-        const currentProductDetail = await Vehicle.findOne({
+        currentVehicle = await Vehicle.findOne({
             where: {
-                placa: Plate
+                placa: datosVehiculo.plate
             },
-            attributes: ['id'] 
+            attributes: ['id'], 
+            transaction 
         });
-        if (!currentProductDetail) {
-            throw new Error('Producto no permitido.');            
+        if (!currentVehicle) {
+            currentVehicle = await ExternalVehicle.findOne({
+                where: {
+                    plate: datosVehiculo.plate
+                },
+                attributes: ['id'], 
+                transaction  
+            });                
         }
-        return currentProductDetail;
+        if (!currentVehicle) {
+            currentVehicle = await ExternalVehicle.create({"plate":datosVehiculo.plate,"idPersonaPropietario":idPersona,"idUsuarioCre":idUsuario, "observacion":"Vehiculo EDS Facturacion"},{ transaction });
+        }
+
+        return currentVehicle;
     } catch (error) {
-        throw new Error('Error al consultar el producto. ('+error+')');
+        throw new Error('Error al gestionar la persona. ('+error+')');
     }
 };
 /**
@@ -32,21 +43,16 @@ const getVehicle = async (Plate) => {
  * @returns {Promise<Object>} - Retorna el objeto Vehicle creado o actualizado.
  */
 const fillVehicle = async (datosVehiculo) => {
-    let vehicle;
-    if(datosVehiculo.Plate ){
-        vehicle = await getVehicle(datosVehiculo.Plate);            
-    }
-    try {    
+    try {
         const productData = {
-            id_vehiculo: datosVehiculo.Plate ? datosVehiculo.Plate : null,
-            placa: datosVehiculo.Plate ? datosVehiculo.Plate : null,
+            plate: datosVehiculo.Plate ? datosVehiculo.Plate : null,
             fecha_ultimo_mantenimiento: datosVehiculo.LastMaintenanceDate ? datosVehiculo.LastMaintenanceDate : null,
             fecha_siguiente_mantenimiento: datosVehiculo.NextMaintenanceDate ? datosVehiculo.NextMaintenanceDate : null,
             kilometros_veh: datosVehiculo.Mileage ? datosVehiculo.Mileage : null
         };
         return productData;
     } catch (error) {
-        throw new Error('Error al llenar la informacion de la producto.'+error);
+        throw new Error('Error al llenar la informacion de la vehiculo.'+error);
     }
 };
 /**
@@ -55,60 +61,15 @@ const fillVehicle = async (datosVehiculo) => {
  * @returns {Promise<boolean>} - Retorna `true` si es valido, `false` de lo contrario.
  */
 const validateVehicle = async (datosVehiculo) => {
-    try {
-        /* Validation With Regex */
-        const regexLetters = /^[A-Za-zÁÉÍÓÚáéíóúÑñ\s]+$/;;
-        const regexNumeric = /^[0-9]+$/;
-        const regexEmail = /^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}$/;  
-        const regexThreeDecimals = /^\d+(\.\d{1,3})?$/;
-            
-        /* VehicleId */
-        if (!datosVehiculo.product_detail_id) {
-            throw new Error('Debe completar el identificador del producto.');                                                                                                                    
-        }      
-        if (!regexNumeric.test(datosVehiculo.product_detail_id)) {
-            throw new Error('El identificador no es valido.');                                                                                                                    
-        }         
-        /* Cantidad */
-        if (!datosVehiculo.cantidad) {
-            throw new Error('Debe completar la cantidad del producto.');                                                                                                                    
-        }      
-        if (!regexThreeDecimals.test(datosVehiculo.cantidad)) {
-            throw new Error('La cantidad no es valida.');                                                                                                                    
-        }         
-        /* Precio */ 
-        if (!datosVehiculo.valor_unitario) {
-            throw new Error('Debe completar el Precio del producto.');                                                                                                                    
-        }      
-        if (!regexThreeDecimals.test(datosVehiculo.valor_unitario)) {
-            throw new Error('El Precio no es valido.');                                                                                                                    
-        }         
-        /* Descuento */ 
-        if (!regexThreeDecimals.test(datosVehiculo.valor_descuento)) {
-            throw new Error('El Descuento no es valido.');                                                                                                                    
-        }         
-        /* Iva */  
-        if (!regexThreeDecimals.test(datosVehiculo.valor_impuesto)) {
-            throw new Error('El Iva no es valido.');                                                                                                                    
-        }             
-        /* Subtotal */ 
-        if (!datosVehiculo.valor_neto) {
-            throw new Error('Debe completar el Subtotal del producto.');                                                                                                                    
-        }      
-        if (!regexThreeDecimals.test(datosVehiculo.valor_neto)) {
-            throw new Error('El Subtotal no es valido.');                                                                                                                    
-        }                    
-        /* Total */ 
-        if (!datosVehiculo.valor_total) {
-            throw new Error('Debe completar el Total del producto.');                                                                                                                    
-        }      
-        if (!regexThreeDecimals.test(datosVehiculo.valor_total)) {
-            throw new Error('El Total no es valido.');                                                                                                                    
-        }           
+    try {        
+        /* Vehicle */
+        if (!datosVehiculo.plate) {
+            throw new Error('Debe completar la placa del vehiculo.');                                                                                                                    
+        }
         return true;
     } catch (error) {
         throw new Error(error);
     }
 };
 
-module.exports = { fillVehicle, validateVehicle };
+module.exports = { fillVehicle, validateVehicle, manageVehicle };
