3e4378dad42194b1de6d21600eb6287635f8eed3
ieztebann <114705067+ieztebann@users.noreply.github.com>
2024-12-03 14:01:30 -0500

    Actualizo modelo para agregar nuevo controlador de pagos Actualizacion continua

--- routes/vtest/controller/index.js
+++ routes/vtest/controller/index.js
@@ -3,6 +3,7 @@ const PersonController = require('./Person');
 const ProductController = require('./Product');
 const OutputController = require('./Output');
 const VehicleController = require('./Vehicle');
+const PaymentController = require('./Payment');
 const UtilController = require('./util');
 
 // Exportar todos los controladores en un solo objeto
@@ -12,5 +13,6 @@ controller.PersonController = PersonController;
 controller.UtilController = UtilController;
 controller.VehicleController = VehicleController;
 controller.OutputController = OutputController;
+controller.PaymentController = PaymentController;
 
 module.exports = controller;
