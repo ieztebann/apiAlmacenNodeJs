9499e1069c24dd26b9abf3a831652bc33a4facac
ieztebann <114705067+ieztebann@users.noreply.github.com>
2024-12-04 09:08:59 -0500

    Corrijo el orden y agrego nuevo controlador de empresa sistema Actualizacion continua

--- routes/vtest/controller/index.js
+++ routes/vtest/controller/index.js
@@ -4,15 +4,17 @@ const ProductController = require('./Product');
 const OutputController = require('./Output');
 const VehicleController = require('./Vehicle');
 const PaymentController = require('./Payment');
+const CreditController = require('./Credit');
 const UtilController = require('./util');
 
 // Exportar todos los controladores en un solo objeto
 const controller = {};
 controller.ProductController = ProductController;
 controller.PersonController = PersonController;
-controller.UtilController = UtilController;
 controller.VehicleController = VehicleController;
 controller.OutputController = OutputController;
 controller.PaymentController = PaymentController;
+controller.CreditController = CreditController;
+controller.UtilController = UtilController;
 
 module.exports = controller;
